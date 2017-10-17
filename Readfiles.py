9dc3042c591108b506ac519d11a508fd5feb9798
baoguaalalei <wenhuixu@tju.edu.cn>
2017-10-17 13:26:56 +0800

    添加文件的保存和读取

--- Readfiles.py
+++ Readfiles.py
@@ -21,10 +21,19 @@ def file2matrix(path,delimiter):
 	# 逐行遍历
 	# 结果按分隔符分割为行向量	
 	recordlist =[ row.split(delimiter) for row in rowlist if row.strip()]
-	return mat(recordlist)	# 返回转换后的矩阵形式	
-root = "D:\\Python\\test\\dataset" #数据文件所在路径
-pathlist = os.listdir(root) # 获取路径下所有数据文件
+	return mat(recordlist)	                           # 返回转换后的矩阵形式	
+root = "D:\\Python\\test\\dataset"                    # 数据文件所在路径，里面包含转义字符
+pathlist = os.listdir(root)                           # 获取路径下所有数据文件
+recordmat = [file2matrix(root+"/"+path,"\t") for path in pathlist]  #文件到矩阵的转换
 
-for path in pathlist:
-    recordmat = file2matrix(root+"/"+path,"\t")
-    print shape(recordmat)
+file_obj = open(root+"/recordmat.dat", "wb")
+pickle.dump(recordmat[0],file_obj)                    # 将生成的矩阵对象保存到指定的位置
+file_obj.close()
+
+read_obj = open(root+"/recordmat.dat", "rb")
+readmat = pickle.load(read_obj)                    # 从指定位置读取对象
+print shape(readmat)
+
+#for path in pathlist:
+#   recordmat = file2matrix(root+"/"+path,"\t")
+#   print shape(recordmat)
