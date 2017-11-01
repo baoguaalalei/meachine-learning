78243a1de5bd502ebf3ddd405e849df6e677318c
baoguaalalei <wenhuixu@tju.edu.cn>
2017-11-01 11:25:13 +0800

    添加注释

--- KD_Tree.py
+++ KD_Tree.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct 31 15:59:22 2017
-
+简单的采用坐标轮换方式选取分割轴，为了更高效的分割空间，也可以计算所有数据点在每个维度上数值的方差，然后选择
+方差最大的维度作为当前节点的划分维度。方差越大，说明这个维度上的数据越不集中（稀疏，分散）
+也就说明了他们不属于一个空间，因此需要在这个维度上进行划分。
 @author: xuwh
 """
 import sys
@@ -17,18 +19,18 @@ class KdNode(object):
  
 class KdTree(object):
     def __init__(self, data):
-        k = len(data[0])  # 数据维度
+        k = len(data[0])                 # 数据维度,如果求长度，直接用len（data）
         
         def CreateNode(split, data_set): # 按第split维划分数据集exset创建KdNode
-            if not data_set:    # 数据集为空
+            if not data_set:             # 数据集为空
                 return None
             # key参数的值为一个函数，此函数只有一个参数且返回一个值用来进行比较
             # operator模块提供的itemgetter函数用于获取对象的哪些维的数据，参数为需要获取的数据在对象中的序号
             #data_set.sort(key=itemgetter(split)) # 按要进行分割的那一维数据排序
-            data_set.sort(key=lambda x: x[split])
-            split_pos = len(data_set) // 2      # //为Python中的整数除法
-            median = data_set[split_pos]        # 中位数分割点             
-            split_next = (split + 1) % k        # cycle coordinates
+            data_set.sort(key=lambda x: x[split]) # 该处直接按照data_set中的第一位数据进行升序排序
+            split_pos = len(data_set) // 2        # //为Python中的整数除法
+            median = data_set[split_pos]          # 中位数分割点             
+            split_next = (split + 1) % k          # cycle coordinates 该点为旋转分割的点，下一次划分变为另外一个维度进行
             
             # 递归的创建kd树
             return KdNode(median, split, 
